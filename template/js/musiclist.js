8552d3c19801db0dc5b30fc00dda5c89b988a389
mlik_git <1468744142@qq.com>
2025-07-22 10:10:27 +0800

    250722

--- template/js/musiclist.js
+++ template/js/musiclist.js
@@ -7,11 +7,25 @@ const musiclist = [
     "cover": "一笑倾城 - 汪苏泷_cover.jpeg"
   },
   {
+    "file": "不潮不用花钱 - 林俊杰.mp3",
+    "title": "不潮不用花钱",
+    "artist": "林俊杰",
+    "duration": 233,
+    "cover": "不潮不用花钱 - 林俊杰_cover.jpeg"
+  },
+  {
     "file": "你若成风 - 许嵩.mp3",
     "title": "你若成风",
     "artist": "许嵩",
     "duration": 173,
     "cover": "你若成风 - 许嵩_cover.jpeg"
+  },
+  {
+    "file": "你若成风 - 许嵩、莫诗旎.mp3",
+    "title": "你若成风",
+    "artist": "许嵩;莫诗旎",
+    "duration": 220,
+    "cover": "你若成风 - 许嵩、莫诗旎_cover.jpeg"
   },
   {
     "file": "十年 - 陈奕迅.mp3",
@@ -21,11 +35,123 @@ const musiclist = [
     "cover": "十年 - 陈奕迅_cover.jpeg"
   },
   {
+    "file": "千百度 - 许嵩.mp3",
+    "title": "千百度",
+    "artist": "许嵩",
+    "duration": 226,
+    "cover": "千百度 - 许嵩_cover.jpeg"
+  },
+  {
+    "file": "南山忆 - 许嵩.mp3",
+    "title": "南山忆",
+    "artist": "许嵩",
+    "duration": 201,
+    "cover": "南山忆 - 许嵩_cover.jpeg"
+  },
+  {
+    "file": "叹服 - 许嵩.mp3",
+    "title": "叹服",
+    "artist": "许嵩",
+    "duration": 261,
+    "cover": "叹服 - 许嵩_cover.jpeg"
+  },
+  {
     "file": "后会无期 - 汪苏泷、徐良.mp3",
     "title": "后会无期",
     "artist": "汪苏泷;徐良",
     "duration": 211,
     "cover": "后会无期 - 汪苏泷、徐良_cover.jpeg"
+  },
+  {
+    "file": "城府 - 许嵩.mp3",
+    "title": "城府",
+    "artist": "许嵩",
+    "duration": 199,
+    "cover": "城府 - 许嵩_cover.jpeg"
+  },
+  {
+    "file": "多余的解释 - 许嵩.mp3",
+    "title": "多余的解释",
+    "artist": "许嵩",
+    "duration": 277,
+    "cover": "多余的解释 - 许嵩_cover.jpeg"
+  },
+  {
+    "file": "天龙八部之宿敌 - 许嵩.mp3",
+    "title": "天龙八部之宿敌",
+    "artist": "许嵩",
+    "duration": 264,
+    "cover": "天龙八部之宿敌 - 许嵩_cover.jpeg"
+  },
+  {
+    "file": "如果当时 - 许嵩.mp3",
+    "title": "如果当时",
+    "artist": "许嵩",
+    "duration": 316,
+    "cover": "如果当时 - 许嵩_cover.jpeg"
+  },
+  {
+    "file": "如约而至 - 许嵩.mp3",
+    "title": "如约而至",
+    "artist": "许嵩",
+    "duration": 255,
+    "cover": "如约而至 - 许嵩_cover.jpeg"
+  },
+  {
+    "file": "山水之间 - 许嵩.mp3",
+    "title": "山水之间",
+    "artist": "许嵩",
+    "duration": 276,
+    "cover": "山水之间 - 许嵩_cover.jpeg"
+  },
+  {
+    "file": "幻听 - 许嵩.mp3",
+    "title": "幻听",
+    "artist": "许嵩",
+    "duration": 273,
+    "cover": "幻听 - 许嵩_cover.jpeg"
+  },
+  {
+    "file": "庐州月 - 许嵩.mp3",
+    "title": "庐州月",
+    "artist": "许嵩",
+    "duration": 255,
+    "cover": "庐州月 - 许嵩_cover.jpeg"
+  },
+  {
+    "file": "惊鸿一面 - 许嵩、黄龄.mp3",
+    "title": "惊鸿一面",
+    "artist": "许嵩;黄龄",
+    "duration": 256,
+    "cover": "惊鸿一面 - 许嵩、黄龄_cover.jpeg"
+  },
+  {
+    "file": "想象之中 - 许嵩.mp3",
+    "title": "想象之中",
+    "artist": "许嵩",
+    "duration": 249,
+    "cover": "想象之中 - 许嵩_cover.jpeg"
+  },
+  {
+    "file": "手心的蔷薇 - 林俊杰、G.E.M. 邓紫棋.mp3",
+    "title": "手心的蔷薇",
+    "artist": "林俊杰;G.E.M. 邓紫棋",
+    "duration": 280,
+    "cover": "手心的蔷薇 - 林俊杰、G.E.M. 邓紫棋_cover.jpeg"
+  },
+  {
+    "file": "摄影艺术 - 许嵩.mp3",
+    "title": "摄影艺术",
+    "artist": "许嵩",
+    "duration": 267,
+    "cover": "摄影艺术 - 许嵩_cover.jpeg"
+  },
+  {
+    "file": "断桥残雪 - 许嵩.mp3",
+    "title": "断桥残雪",
+    "artist": "许嵩",
+    "duration": 227,
+    "cover": "断桥残雪 - 许嵩_cover.jpeg"
   },
   {
     "file": "星晴 - 周杰伦.mp3",
@@ -35,11 +161,39 @@ const musiclist = [
     "cover": "星晴 - 周杰伦_cover.jpeg"
   },
   {
+    "file": "最佳许嵩 - 歌手.mp3",
+    "title": "最佳歌手",
+    "artist": "许嵩",
+    "duration": 267,
+    "cover": "最佳许嵩 - 歌手_cover.jpeg"
+  },
+  {
+    "file": "有何不可 - 许嵩.mp3",
+    "title": "有何不可",
+    "artist": "许嵩",
+    "duration": 241,
+    "cover": "有何不可 - 许嵩_cover.jpeg"
+  },
+  {
     "file": "枫 - 周杰伦.mp3",
     "title": "枫",
     "artist": "周杰伦",
     "duration": 275,
     "cover": "枫 - 周杰伦_cover.jpeg"
+  },
+  {
+    "file": "江南 - 林俊杰.mp3",
+    "title": "江南",
+    "artist": "林俊杰",
+    "duration": 267,
+    "cover": "江南 - 林俊杰_cover.jpeg"
+  },
+  {
+    "file": "清明雨上 - 许嵩.mp3",
+    "title": "清明雨上",
+    "artist": "许嵩",
+    "duration": 219,
+    "cover": "清明雨上 - 许嵩_cover.jpeg"
   },
   {
     "file": "演员 - 薛之谦.mp3",
@@ -49,11 +203,32 @@ const musiclist = [
     "cover": "演员 - 薛之谦_cover.jpeg"
   },
   {
+    "file": "灰色头像 - 许嵩.mp3",
+    "title": "灰色头像",
+    "artist": "许嵩",
+    "duration": 289,
+    "cover": "灰色头像 - 许嵩_cover.jpeg"
+  },
+  {
+    "file": "燕归巢 - 许嵩.mp3",
+    "title": "燕归巢",
+    "artist": "许嵩",
+    "duration": 294,
+    "cover": "燕归巢 - 许嵩_cover.jpeg"
+  },
+  {
     "file": "犯贱 - 徐良、阿悄.mp3",
     "title": "犯贱",
     "artist": "徐良;阿悄",
     "duration": 213,
     "cover": "犯贱 - 徐良、阿悄_cover.jpeg"
+  },
+  {
+    "file": "玫瑰花的葬礼 - 许嵩.mp3",
+    "title": "玫瑰花的葬礼",
+    "artist": "许嵩",
+    "duration": 260,
+    "cover": "玫瑰花的葬礼 - 许嵩_cover.jpeg"
   },
   {
     "file": "突然好想你 - 五月天.mp3",
@@ -96,5 +271,19 @@ const musiclist = [
     "artist": "蔡依林",
     "duration": 226,
     "cover": "说爱你 - 蔡依林_cover.jpeg"
+  },
+  {
+    "file": "野路 - 许嵩.mp3",
+    "title": "野路",
+    "artist": "许嵩",
+    "duration": 249,
+    "cover": "野路 - 许嵩_cover.jpeg"
+  },
+  {
+    "file": "雅俗共赏 - 许嵩.mp3",
+    "title": "雅俗共赏",
+    "artist": "许嵩",
+    "duration": 249,
+    "cover": "雅俗共赏 - 许嵩_cover.jpeg"
   }
 ];
